e17b6ba3a45873e3abb15364ab60564cf8699364
Izidor Matušov <izidor.matusov@gmail.com>
2013-10-06 17:52:39 +0100

    Parse transactions

--- lloyds.py
+++ lloyds.py
@@ -1,5 +1,85 @@
 import mechanize
 import re
+from decimal import Decimal as D
+from datetime import date, datetime
+
+class Transaction(object):
+    """ Single transaction """
+
+    EXPLANATIONS = {
+        'BGC': 'Bank giro credit',
+        'BP': 'Bill payment',
+        'CD': 'Card payment * (followed by the last 4 digits of the card)',
+        'CHG': 'Charge',
+        'CHQ': 'Cheque',
+        'COMM': 'Commission',
+        'COR': 'Correction',
+        'CPT': 'Cashpoint',
+        'CSH': 'Cash',
+        'CSQ': 'Cash / Cheque',
+        'DD': 'Direct Debit',
+        'DEB': 'Debit card',
+        'DEP': 'Deposit',
+        'DR': 'Overdrawn balance',
+        'EUR': 'Euro cheque',
+        'FPI': 'Faster payments incoming',
+        'FPO': 'Faster payments outgoing',
+        'IB': 'Internet Banking',
+        'MTU': 'Mobile top up',
+        'PAY': 'Payment',
+        'PSV': 'Paysave',
+        'SAL': 'Salary',
+        'SO': 'Standing order',
+        'TFR': 'Transfer',
+    }
+
+    def __init__(self, fields):
+        dt = datetime.strptime(fields['Transaction Date'], '%d/%m/%Y')
+        self.date = date(dt.year, dt.month, dt.day)
+
+        self.transaction_type = fields['Transaction Type']
+
+        self.account_number = fields['Account Number']
+        self.sort_code = fields['Sort Code'].strip("'")
+
+        if fields['Debit Amount']:
+            self.amount = -D(fields['Debit Amount'])
+        else:
+            self.amount = D(fields['Credit Amount'])
+
+        self.balance = D(fields['Balance'])
+        self.card = None
+
+        self.raw_description = fields['Transaction Description']
+        self.description = self._parse_description()
+
+    def _parse_description(self):
+        """ Parse the cryptic description:
+            - remove date from end
+            - remove card detail
+        """
+        desc = self.raw_description.strip()
+        end_date = self.date.strftime("%d%b%y").upper()
+        if desc.endswith(end_date):
+            desc = desc[:-len(end_date)].strip()
+        match = re.match('(.*?) CD (\d{4})$', desc)
+        if match:
+            desc = match.group(1).strip()
+            self.card = match.group(2)
+        return desc
+
+    def __str__(self):
+        return "Transaction(%s, %s, %s, %s)" % (
+            self.date.strftime('%d/%m/%Y'), self.get_type_explanation(),
+            self.description, self.amount)
+
+    def get_type_explanation(self):
+        """ Return explanation of type """
+        if self.transaction_type.startswith('CD'):
+            four_digits = self.transaction_type[len('CD '):]
+            return 'Paid by card **** **** **** %d' % four_digits
+        else:
+            return self.EXPLANATIONS.get(self.transaction_type, '')
 
 
 class LloydsBank(object):
@@ -52,11 +132,11 @@ class LloydsBank(object):
         self._agent.follow_link(export_link)
         self._agent.select_form('frmTest')
         self._agent.submit()
-        return self._agent.response().read()
 
-
-if __name__ == "__main__":
-    bank = LloydsBank()
-    bank.login('id', 'password', 'secret')
-    for account_url in bank.accounts:
-        print bank.get_transactions(account_url)
+        transactions = []
+        csv = self._agent.response().read().splitlines()
+        header = csv[0].split(',')
+        for row in csv[1:]:
+            fields = dict(zip(header, row.split(',')))
+            transactions.append(Transaction(fields))
+        return transactions
